9d9ff7bfb3e65578907589b3371fa1bf4e609425
Jaakko Husso <jaakko.husso@futurice.com>
2017-03-11 01:26:42 +0200

    Fetch images for board members.

--- src/config/config.js
+++ src/config/config.js
@@ -4,6 +4,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 const config = {
   directusApiKey: process.env.DIRECTUS_API_KEY,
   directusApiUrl: process.env.DIRECTUS_API_URL,
+  directusFilesUrl: process.env.DIRECTUS_FILES_URL,
   port: Number(process.env.PORT || 8080),
 };
 
--- src/directus/cms.js
+++ src/directus/cms.js
@@ -1,4 +1,4 @@
-// const config = require('../config/config');
+const config = require('../config/config');
 const dataTypes = require('./data-types');
 const directus = require('./client');
 const R = require('ramda');
@@ -10,32 +10,32 @@ const client = directus.getDirectusClient();
 const ACTIVE = 1;
 const perPage = 25;
 
-function getActiveItems(dataType, options) {
+const getActiveItems = (dataType, options) => {
   const defaultParams = {
     status: ACTIVE,
   };
   const params = R.merge(defaultParams, options);
 
   return client.getItems(dataType, params);
-}
+};
 
-function getActiveItem(dataType, id, options) {
+const getActiveItem = (dataType, id, options) => {
   const defaultParams = {
     status: ACTIVE,
   };
   const params = R.merge(defaultParams, options);
 
   return client.getItem(dataType, id, params);
-}
+};
 
-function getActiveFile(id, options) {
+const getActiveFile = (id, options) => {
   const defaultParams = {
     status: ACTIVE,
   };
   const params = R.merge(defaultParams, options);
 
   return client.getFile(id, params);
-}
+};
 
 const fetchNews = (currentPage) => {
   const options = {
@@ -85,6 +85,12 @@ const fetchPastEvents = (currentPage) => {
 
 const fetchBoardMember = memberId => getActiveItem(dataTypes.boardMembers.table, memberId);
 
+const fetchBoardMemberImage = member => BPromise.props(
+  R.merge(member, {
+    image: getActiveFile(R.prop(['image'], member))
+      .then(image => config.directusFilesUrl + R.path(['data', 'thumbnail_url'], image)),
+  }));
+
 const fetchGuildBoards = () => {
   const options = {
     sort: 'year',
@@ -108,12 +114,14 @@ const fetchGuildBoardByYear = (year) => {
 
   return getActiveItems(dataTypes.guildBoard.table, options)
     .then(R.compose(R.head, R.prop('data')))
-    .then(board => BPromise.props({
-      meta: board,
-      // TODO: assign or some shit here, also fetch images for ALL board members.
-      chairman: fetchBoardMember(board.chairman).then(chairman => getActiveFile(chairman.image)),
-    }))
-    .then(utils.mapGuildBoard);
+    .then(data => BPromise.props({
+      meta: R.pick(['text', 'title', 'year', 'slug', 'board_members_title', 'board_officials_title'], data),
+      chairman: fetchBoardMember(data.chairman)
+        .then(R.prop('data'))
+        .then(fetchBoardMemberImage),
+      board_members: BPromise.map(R.path(['board_members', 'data'], data), fetchBoardMemberImage),
+      board_officials: BPromise.map(R.path(['board_officials', 'data'], data), fetchBoardMemberImage),
+    }));
 };
 
 module.exports = {
